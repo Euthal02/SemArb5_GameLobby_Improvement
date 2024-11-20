11d29bad940657c1555f1c193b45878e930af060
meuthak <marco.kaelin1@edu.tbz.ch>
2024-11-20 16:28:10 +0100

    working version locally

--- gameroom/README.md
+++ gameroom/README.md
@@ -1,3 +1,9 @@
 # Game Room
 
 In diesem Unterordner wird der Game Room entwickelt.
+
+Um den Container lokal zu testen, ist dieser Command hilfreich:
+
+```bash
+docker compose -f compose.yaml up --build
+```
--- gameroom/app/__init__.py
+++ gameroom/app/__init__.py
@@ -1,87 +1,70 @@
-from flask import Flask, render_template
-from flask import abort as flask_abort
-from flask_socketio import SocketIO
-from flask import request, session
-from flask import make_response
-from random import randrange
-from game import PongGame
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit
+from app.game_update import update_game, get_random_ball_speed
 
-# Flask App initialisieren
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+# Game state
+game_state = {
+    # ball speed should be random
+    "ball": {"x": 300, "y": 200, "dx": get_random_ball_speed(), "dy": get_random_ball_speed()},
+    "paddles": {"player1": 160, "player2": 160},
+    "scores": {"player1": 0, "player2": 0},
+    "width": 700,
+    "height": 500,
+    "paddle_height": 90,
+    "paddle_width": 10,
+}
 
-# this class handles the user sessions
-class UserSessions:
-    def __init__(self):
-        self.all_user_sessions = {}
+players = {}
 
-    def create_user_session(self):
-        # create random number
-        session_id = randrange(start=100000,stop=1000000)
-        return session_id
+def setup_game():
+    app = Flask(__name__)
+
+    # this just returns the basic index.html
+    @app.route("/")
+    def index():
+        return render_template("game.html")
     
-    def number_of_sessions(self):
-        return len(self.all_user_sessions.keys())
+    socketio = SocketIO(app)
 
-# movement from players
-@socketio.on('move')
-def handle_move(data):
-    current_game.game_move(data)
-
-# frontend
-@app.route('/')
-def index():
-    # identify if this user is already "logged" in
-    if not request.cookies.get("sid"):
-        sid = user_session.create_user_session()
-    else:
-        sid = request.cookies.get("sid")
-
-    if len(user_session.all_user_sessions.keys()) <= 2:
-        # find out if the client is already connected or not
-        if sid in user_session.all_user_sessions.values():
-            pass
-        else:
-            if 'player1' not in user_session.all_user_sessions:
-                user_session.all_user_sessions['player1'] = sid
-            elif 'player2' not in user_session.all_user_sessions:
-                user_session.all_user_sessions['player2'] = sid
+    @socketio.on("connect")
+    def on_connect():
+        global players
+        if len(players.keys()) < 2:
+            if 1 not in players.values():
+                player_id = 1
             else:
-                flask_abort(401, "Maximum amount of Players reached!")
-        
-        # return the identifier to the client
-        session['sid'] = sid
-        resp = make_response(render_template('index.html'))
-        resp.set_cookie("sid", str(sid))
-        return resp
-    else:
-        flask_abort(401, "Maximum amount of Players reached!")
+                player_id = 2
 
-# start the game in the background
-@socketio.on('connect')
-def start_game_loop():
-    sid = request.cookies.get("sid")
-    session['sid'] = sid
+            players[request.sid] = player_id
+            # the room parameter just defines the scope of this reply.
+            emit("player_id", player_id, room=request.sid)
+            print(f"Player {player_id} connected: {request.sid}")
+        else:
+            emit("full", room=request.sid)
+
+
+    @socketio.on("disconnect")
+    def on_disconnect():
+        global players
+        if request.sid in players.keys():  
+            print(f"Player {players[request.sid]} disconnected: {request.sid}")
+            del players[request.sid]
+
+
+    @socketio.on("move_paddle")
+    def move_paddle(data):
+        player = data["player"]
+        position = data["position"]
+
+        # update the paddles
+        game_state["paddles"][player] = max(0, min(game_state["height"] - game_state["paddle_height"], position))
+
+    # Background game loop
+    def game_loop():
+        while True:
+            update_game(game_state_dict=game_state, socketio_object=socketio)
+            socketio.sleep(0.03)  # 30 FPS
+
     socketio.start_background_task(game_loop)
 
-# remove players when they are gone.
-@socketio.on('disconnect')
-def on_disconnect():
-    sid = request.cookies.get("sid")
-    if sid == user_session.all_user_sessions.get('player1'):
-        del user_session.all_user_sessions['player1']
-    elif sid == user_session.all_user_sessions.get('player2'):
-        del user_session.all_user_sessions['player2']
-
-def game_loop():
-    while True:
-        # 30 milisecond buffer
-        current_game.update()
-
-# start the game
-if __name__ == '__main__':
-    user_session = UserSessions()
-    current_game = PongGame(socketio, user_session, session)
-    socketio.run(app, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)
-    
+    return app
