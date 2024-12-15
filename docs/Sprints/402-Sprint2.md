f704c93c4d200d3013059a82aaa694ee7654127c
meuthak <marco.kaelin1@edu.tbz.ch>
2024-12-15 19:22:18 +0100

    sprint abschluss 2

--- docs/Sprints/402-Sprint2.md
+++ docs/Sprints/402-Sprint2.md
@@ -16,16 +16,46 @@ Hier sind die Erkenntnisse des zweiten Sprints.
 
 ## Was ist gut gelaufen? Was konnte bereits erreicht werden?
 
+Ich konnte im zweiten Sprint sehr gut vorankommen und habe für den GameRoom und die GameLobby ein MVP erstellt. Für die letzten Tests ist mir jedoch aufgefallen, dass die Produktiv Umgebung noch nicht ganz miteinander harmoniert. Lokal funktioniert jedoch alles sehr gut. Das ganze EKS Setup konnte ich automatisieren und durch die konstante Löschung und Wiederaufbauung des Cluster konnte ich Kosten sparen.
+
+Die Images konnte ich sehr gut auf den Cluster deployen mit Helm. Das ganze ist noch nicht ganz ausgereift um in die Pipeline integriert zu werden. Aber ich habe somit also bereits einen guten Weg um das ganze zu deployen.
+
+Die Entwicklung verlief sehr gut, ich konnte grosse Fortschritte erreichen und meine Ziele einfach erreichen. Bezüglich der Arbeitsweise benötigt es also keine Änderung.
+
 ## Was hat nicht funktioniert? Was war nicht gut? Welche Aspekte sollen entfallen?
+
+Wie bereits erwähnt, ist die Pipeline noch nicht ganz ausgereift. Auch die Zusammenarbeit zwischen dem Room und der Lobby funktioniert in der Produktivumgebung noch nicht wie gewollt. Das ganze benötigt noch einiges an Entwicklung.
 
 ## Was soll aufgrund der gemachten Erfahrungen ausprobiert werden?
 
+Ich möchte gerne weiterarbeiten so wie bisher.
+
 ## Erledigte Tasks des letzten Sprints?
+
+Ich konnte die folgenden Tasks erledigen. Mehr Infos zu den einzelnen Tasks finden sich in den angefügten Links:
+
+[Kubernetes Cluster mit GHCR verbinden](https://github.com/Euthal02/SemArb4_GameLobby/issues/10)
+[GameLobby selbst schreiben](https://github.com/Euthal02/SemArb4_GameLobby/issues/9)
+[Eigenes GameRoom Image erstellen](https://github.com/Euthal02/SemArb4_GameLobby/issues/7)
+[Image für die GameLobby in den Grundzügen erstellt](https://github.com/Euthal02/SemArb4_GameLobby/issues/23)
+[Image für den Game Room in den Grunzügen erstellt](https://github.com/Euthal02/SemArb4_GameLobby/issues/24)
 
 ## Momentaufnahme Backlog / Projektstatus
 
+![Backlog bei Abschluss Sprint 2](../ressources/images/projektmanagement/backlog2.PNG)
+
+Ich habe noch 10 Tasks offen. Dies sind 5 Tasks weniger als beim letzten Sprint. Dies ist ein guter Fortschritt.
+
 ## Warum ist der kommende Sprint wichtig?
+
+Im kommenden Sprint möchte ich das Zusammenspiel zwischen der Lobby und dem Room verbessern. Auch möchte ich damit beginnen das ganze in der Pipeline zu deployen. Dies erleichtert es mir das ganze zu finalisieren.
 
 ## Was muss ich tun, um das Sprintziel zu erreichen?
 
+Ich muss weiterhin so arbeiten wie bisher und mich an den Zeitplan halten und die Tasks abarbeiten. Wenn das ganze so weiterläuft wie im letzten Sprint, sehe ich das sehr positiv.
+
 ## Blick auf den 3. Sprint
+
+![Sprint 3 - Vorrausblick](../ressources/images/projektmanagement/sprint3_vorrausblick.PNG)
+
+Hier sind alle noch offenen Tasks des dritten Sprints.
--- eks/build.sh
+++ eks/build.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
+CLUSTER_NAME=eks-cluster
+REGION=eu-central-2
+
 # eksctl
-eksctl create cluster --name=eks-cluster --region=eu-central-2 --node-ami-family=AmazonLinux2 --nodes=2 --nodes-min=1 --nodes-max=3 --ssh-access --ssh-public-key=semesterarbeit_admin_access --max-pods-per-node=20 --enable-ssm --with-oidc
-eksctl utils update-cluster-vpc-config --cluster=eks-cluster --public-access-cidrs=45.94.88.37/32 --private-access=true --approve
-eksctl upgrade cluster --name=eks-cluster --approve
+eksctl create cluster --name=$CLUSTER_NAME --region=$REGION --node-ami-family=AmazonLinux2 --nodes=2 --nodes-min=1 --nodes-max=3 --ssh-access --ssh-public-key=semesterarbeit_admin_access --max-pods-per-node=20 --enable-ssm --with-oidc
+eksctl utils update-cluster-vpc-config --cluster=$CLUSTER_NAME --public-access-cidrs=45.94.88.37/32 --private-access=true --approve
+eksctl upgrade cluster --name=$CLUSTER_NAME --approve
 
 # get account id
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text)
 
 # remove remainders from old deployments
 aws iam delete-policy --policy-arn=arn:aws:iam::"$AWS_ACCOUNT_ID":policy/AWSLoadBalancerControllerIAMPolicy
+aws iam delete-policy --policy-arn=arn:aws:iam::"$AWS_ACCOUNT_ID":policy/AllowExternalDNSUpdates
 
 # alb in k8s
 curl -O https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/refs/heads/main/docs/install/iam_policy.json
 aws iam create-policy --policy-name AWSLoadBalancerControllerIAMPolicy --policy-document file://iam_policy.json --no-cli-pager
-eksctl create iamserviceaccount --cluster=eks-cluster --namespace=kube-system --name=aws-load-balancer-controller --role-name AmazonEKSLoadBalancerControllerRole --attach-policy-arn=arn:aws:iam::"$AWS_ACCOUNT_ID":policy/AWSLoadBalancerControllerIAMPolicy --approve --override-existing-serviceaccounts
+eksctl create iamserviceaccount --cluster=$CLUSTER_NAME --namespace=kube-system --name=aws-load-balancer-controller --role-name AmazonEKSLoadBalancerControllerRole --attach-policy-arn=arn:aws:iam::"$AWS_ACCOUNT_ID":policy/AWSLoadBalancerControllerIAMPolicy --approve --override-existing-serviceaccounts
 
 # install helm for preconfigured aws loadbalancer
 helm repo add eks https://aws.github.io/eks-charts
@@ -22,6 +26,15 @@ helm install aws-load-balancer-controller eks/aws-load-balancer-controller -n ku
 wget https://raw.githubusercontent.com/aws/eks-charts/master/stable/aws-load-balancer-controller/crds/crds.yaml
 kubectl apply -f crds.yaml
 
+# install external dns
+# aws iam create-policy --policy-name "AllowExternalDNSUpdates" --policy-document file://eks/external-dns-policy.conf
+# POLICY_ARN=$(aws iam list-policies --query 'Policies[?PolicyName==`AllowExternalDNSUpdates`].Arn' --output text)
+# eksctl create iamserviceaccount --cluster=$CLUSTER_NAME --name=external-dns --namespace=default --attach-policy-arn=$POLICY_ARN --approve --override-existing-serviceaccounts
+# wget https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/main/docs/examples/external-dns.yaml
+# sed -i 's/--domain-filter=external-dns-test\.my-org\.com/--domain-filter=semesterarbeit.com/g' external-dns.yaml
+# kubectl apply -f external-dns.yaml
+
 # remove downloaded files
 rm -f crds.yaml
 rm -f iam_policy.json
+rm -f external-dns.yaml
